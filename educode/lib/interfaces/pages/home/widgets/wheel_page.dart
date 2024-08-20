9d87a8508302baae53a7de77c511c12d0f71d73e
Jose Baca <joba1521@gmail.com>
2024-08-20 14:52:32 -0600

    se corrgieron detalles en wheel page y controller multijugador

--- educode/lib/interfaces/pages/home/widgets/wheel_page.dart
+++ educode/lib/interfaces/pages/home/widgets/wheel_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_fortune_wheel/flutter_fortune_wheel.dart';
 import 'package:get/get.dart';
 import 'package:educode/controllers/quizz_controller_multijugador.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class WheelPage extends StatefulWidget {
   final String player1Name;
@@ -27,7 +28,8 @@ class _WheelPageState extends State<WheelPage> {
     "Programación",
     "POO",
     "Algoritmo",
-    "Flutter"
+    "Flutter",
+    "Elige un tema" // Nuevo ítem para la selección de temas
   ];
 
   final StreamController<int> selected = StreamController<int>();
@@ -42,7 +44,6 @@ class _WheelPageState extends State<WheelPage> {
     super.dispose();
   }
 
-  // Seleccionar un nuevo índice para la rueda
   void _selectNewIndex() {
     do {
       selectedIndex = Fortune.randomInt(0, topics.length);
@@ -50,99 +51,136 @@ class _WheelPageState extends State<WheelPage> {
     previousIndex = selectedIndex;
   }
 
-  // Mostrar el diálogo de preguntas
-  void _showQuestionDialog() {
-    Get.defaultDialog(
-      barrierDismissible: false, // No se puede cerrar tocando fuera
-      title: "Pregunta",
-      content: Obx(() {
-        if (quizzController.currentQuestion.value.isEmpty ||
-            quizzController.currentOptions.isEmpty) {
-          return Center(child: CircularProgressIndicator());
-        } else {
-          final optionLetters = ['a', 'b', 'c', 'd'];
-          return Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                quizzController.currentQuestion.value,
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(height: 20),
-              ...List.generate(quizzController.currentOptions.length, (index) {
-                final letter = optionLetters[index];
-                final option = quizzController.currentOptions[index];
+  void _showTopicSelectionDialog() {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return WillPopScope(
+          // ignore: deprecated_member_use
+          onWillPop: () async => false, // Bloquea la acción de retroceso
+          child: AlertDialog(
+            title: Text("Selecciona un tema"),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: topics.sublist(0, topics.length - 1).map((topic) {
                 return Padding(
                   padding: const EdgeInsets.symmetric(vertical: 5.0),
                   child: ElevatedButton(
                     onPressed: () {
-                      quizzController.checkAnswer(letter, widget.player1Name, widget.player2Name);
                       Navigator.of(context).pop();
-                      if (quizzController.player1Stars.value == 5 || quizzController.player2Stars.value == 5) {
-                        Get.defaultDialog(
-                          barrierDismissible: false, // No se puede cerrar tocando fuera
-                          title: "GANADOR",
-                          content: Obx(() {
-                            return Column(
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                Text(
-                                  "Juego terminado ganador ${quizzController.player1Stars.value == 5 ? widget.player1Name : widget.player2Name}",
-                                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                                ),
-                                SizedBox(height: 20),
-                                ElevatedButton(
-                                  onPressed: () {
-                                    quizzController.gameEnded.value = true;
-                                    Get.back();
-                                    Get.back();
-                                  },
-                                  style: ElevatedButton.styleFrom(
-                                    backgroundColor: Colors.grey[600],
-                                    shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(12),
-                                    ),
-                                    minimumSize: Size(double.infinity, 50),
-                                  ),
-                                  child: Text("Volver"),
-                                ),
-                              ],
-                            );
-                          }),
-                        );
-                      }
+                      _loadSelectedTopic(topic);
                     },
                     style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.grey[300],
-                      foregroundColor: Colors.black,
+                      backgroundColor: Colors.blue,
+                      foregroundColor: Colors.white,
                       minimumSize: Size(double.infinity, 50),
                       shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(12),
+                        borderRadius: BorderRadius.circular(10),
                       ),
                     ),
                     child: Text(
-                      '$letter. $option',
+                      topic,
                       style: TextStyle(fontSize: 16),
                     ),
                   ),
                 );
-              }),
+              }).toList(),
+            ),
+            actions: [
+              TextButton(
+                child: Text("Cancelar"),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
             ],
-          );
-        }
-      }),
+          ),
+        );
+      },
     );
   }
 
-  // Construir las estrellas de progreso de cada jugador
+  void _loadSelectedTopic(String topic) {
+    quizzController.loadQuestions(topic).then((_) {
+      if (quizzController.currentQuestion.value.isNotEmpty) {
+        _showQuestionDialog();
+      } else {
+        print('No se pudo cargar la pregunta para el tema $topic');
+      }
+    });
+  }
+
+  void _showQuestionDialog() {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return WillPopScope(
+          // ignore: deprecated_member_use
+          onWillPop: () async => false, // Bloquea la acción de retroceso
+          child: AlertDialog(
+            title: Text("Pregunta"),
+            content: Obx(() {
+              if (quizzController.currentQuestion.value.isEmpty ||
+                  quizzController.currentOptions.isEmpty) {
+                return Center(child: CircularProgressIndicator());
+              } else {
+                final optionLetters = ['a', 'b', 'c', 'd'];
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      quizzController.currentQuestion.value,
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 20),
+                    ...List.generate(quizzController.currentOptions.length,
+                        (index) {
+                      final letter = optionLetters[index];
+                      final option = quizzController.currentOptions[index];
+                      return Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 5.0),
+                        child: ElevatedButton(
+                          onPressed: () {
+                            quizzController.checkAnswer(
+                                letter, widget.player1Name, widget.player2Name);
+                            Navigator.of(context).pop();
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.white,
+                            foregroundColor: Colors.black,
+                            minimumSize: Size(double.infinity, 50),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                          child: Text(
+                            '$letter. $option',
+                            style: TextStyle(fontSize: 16),
+                          ),
+                        ),
+                      );
+                    }),
+                  ],
+                );
+              }
+            }),
+          ),
+        );
+      },
+    );
+  }
+
   Widget _buildStars(int filledStars) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: List.generate(5, (index) {
         return Icon(
           index < filledStars ? Icons.star : Icons.star_border,
-          color: index < filledStars ? Colors.amber : Colors.grey,
-          size: 24,
+          color: index < filledStars ? Colors.yellow : Colors.grey,
         );
       }),
     );
@@ -153,10 +191,8 @@ class _WheelPageState extends State<WheelPage> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Rueda de Temas'),
-        backgroundColor: Colors.grey[800],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
+      body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
@@ -164,12 +200,13 @@ class _WheelPageState extends State<WheelPage> {
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                _buildPlayerInfo(widget.player1Icon, widget.player1Name, quizzController.player1Stars),
-                Text(
-                  'VS',
-                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.grey[700]),
-                ),
-                _buildPlayerInfo(widget.player2Icon, widget.player2Name, quizzController.player2Stars),
+                _buildPlayerInfo(widget.player1Icon, widget.player1Name,
+                    quizzController.player1Stars),
+                Text('VS',
+                    style:
+                        TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+                _buildPlayerInfo(widget.player2Icon, widget.player2Name,
+                    quizzController.player2Stars),
               ],
             ),
             SizedBox(height: 20),
@@ -183,39 +220,31 @@ class _WheelPageState extends State<WheelPage> {
                     items: [
                       for (var topic in topics)
                         FortuneItem(
-                          child: Text(
-                            topic,
-                            style: TextStyle(
-                                fontSize: 18, fontWeight: FontWeight.bold, color: Colors.white),
-                          ),
-                          style: FortuneItemStyle(
-                            color: Colors.grey[500]!,
-                            borderColor: Colors.grey[700]!,
-                            borderWidth: 2,
-                          ),
+                          child: topic == "Elige un tema"
+                              ? FaIcon(FontAwesomeIcons.user, size: 24)
+                              : Text(
+                                  topic,
+                                  style: TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold),
+                                ),
                         ),
                     ],
                     onAnimationEnd: () {
                       final selectedTopic = topics[selectedIndex];
-                      quizzController.loadQuestions(selectedTopic).then((_) {
-                        if (quizzController.currentQuestion.value.isNotEmpty) {
-                          _showQuestionDialog();
-                        } else {
-                          print('No se pudo cargar la pregunta para el tema $selectedTopic');
-                        }
-                      });
+                      if (selectedTopic == "Elige un tema") {
+                        _showTopicSelectionDialog();
+                      } else {
+                        _loadSelectedTopic(selectedTopic);
+                      }
                     },
                   ),
-                  Positioned(
-                    bottom: 20,
-                    child: FloatingActionButton(
-                      onPressed: () {
-                        _selectNewIndex();
-                        selected.add(selectedIndex);
-                      },
-                      backgroundColor: Colors.grey[800],
-                      child: Icon(Icons.rotate_right, size: 30, color: Colors.white),
-                    ),
+                  ElevatedButton(
+                    onPressed: () {
+                      _selectNewIndex();
+                      selected.add(selectedIndex);
+                    },
+                    child: Text('Girar'),
                   ),
                 ],
               ),
@@ -226,14 +255,11 @@ class _WheelPageState extends State<WheelPage> {
     );
   }
 
-  // Construir la información del jugador
   Widget _buildPlayerInfo(IconData icon, String name, RxInt stars) {
     return Column(
       children: [
-        Icon(icon, size: 50, color: Colors.grey[800]),
-        SizedBox(height: 8),
-        Text(name, style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.grey[700])),
-        SizedBox(height: 8),
+        Icon(icon, size: 40),
+        Text(name),
         Obx(() => _buildStars(stars.value)),
       ],
     );
